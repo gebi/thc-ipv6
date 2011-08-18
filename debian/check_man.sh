99446f5912475a5f762a2496208211ceae0ab18f
Michael Gebetsroither <michael@mgeb.org>
2011-08-18 20:48:29 +0200

    add additional output to check_man.sh

--- debian/check_man.sh
+++ debian/check_man.sh
@@ -7,12 +7,24 @@ for i in $tainted_man_tools_; do
     test -f ${i} && man_tools_="$man_tools_ $i"
 done
 
+missing_tools_=""
 for i in `find -maxdepth 1 -perm '-u=x' -type f -printf '%f\n' |sort`; do
     found_="false"
     for j in $man_tools_; do
         if [[ $i == $j ]]; then found_="true"; break; fi
     done
     if [[ $found_ == "false" ]]; then
-        echo $i
+        missing_tools_="$missing_tools_ $i"
     fi
+done
+
+echo "### List"
+for i in $missing_tools_; do echo $i; done
+echo
+echo "### Manpage entries"
+for i in $missing_tools_; do echo .TP; echo ".B $i"; done
+echo
+echo "### dh_link entries"
+for i in $missing_tools_; do
+    echo "usr/share/man/man8/thc-ipv6.8.gz usr/share/man/man8/${i}.8.gz \\"
 done
